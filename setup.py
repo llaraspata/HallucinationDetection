e6cd7c325db1fedb437c04c4134799e365a0b3fe
Lucrezia Laraspata <l.laraspata3@studenti.uniba.it>
2025-07-07 22:03:04 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.1.0",
+    author="Lucrezia Laraspata",
+    author_email="l.laraspata3@phd.uniba.it",
+    description=r'Detect hallucinations in LLMs',
+    packages=find_packages(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/llaraspata/HallucinationDetection.git",
+    python_requires='>=3.9',
+)
