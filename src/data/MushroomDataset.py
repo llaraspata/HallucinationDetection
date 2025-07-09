02c3a102e5cc74642a74d81cb93b6eb83d6ce3da
Lucrezia Laraspata <l.laraspata3@studenti.uniba.it>
2025-07-09 21:28:34 +0200

    methods to add instance id and get language

--- src/data/MushroomDataset.py
+++ src/data/MushroomDataset.py
@@ -5,6 +5,11 @@ from torch.utils.data import Dataset
 class MushroomDataset(Dataset):
     def __init__(self, data_path, data_extension="json"):
         self.dataset = load_dataset(data_extension, data_files=data_path)['train']      # Dummy split, it can be val or test, too
+
+        if 'instance_id' not in self.dataset.column_names:
+            self.dataset = self.create_instance_ids()
+            self.save_dataset_as_jsonl(output_path=data_path)
+        
         self.dataset = self.dataset.shuffle(seed=42)
 
 
@@ -15,5 +20,23 @@ class MushroomDataset(Dataset):
     def __getitem__(self, idx):
         question = self.dataset[idx]['model_input']
         answer = self.dataset[idx]['model_output_text']
-        lang = self.dataset[idx]['lang']
-        return question, answer, lang
+        id = self.dataset[idx]['instance_id']
+        return question, answer, id
+    
+
+    def get_language_by_instance_id(self, instance_id):
+        matches = self.dataset.filter(lambda x: x["instance_id"] == instance_id)
+
+        if len(matches) == 0:
+            raise ValueError(f"Instance ID {instance_id} non trovato.")
+        
+        return matches[0]["lang"]
+
+    
+    def create_instance_ids(self):
+        self.dataset = self.dataset.add_column("instance_id", [i for i in range(len(self.dataset))])
+        return self.dataset
+
+
+    def save_dataset_as_jsonl(self, output_path):
+        self.dataset.to_json(output_path, lines=True, orient="records")
